d393355bd500d64011ac7499b6ed943c4f97e364
iniguezg <iniguez.gerardo@gmail.com>
2021-10-28 17:47:18 +0200

    updated devs script

--- figures/figure_supp_deviations.py
+++ figures/figure_supp_deviations.py
@@ -59,11 +59,11 @@ if __name__ == "__main__":
 	fig_props = { 'fig_num' : 1,
 	'fig_size' : (10, 6),
 	'aspect_ratio' : (4, 6),
-	'grid_params' : dict( left=0.09, bottom=0.08, right=0.995, top=0.93, wspace=0.3, hspace=0.6 ),
+	'grid_params' : dict( left=0.09, bottom=0.08, right=0.995, top=0.9, wspace=0.3, hspace=0.8 ),
 	'dpi' : 300,
 	'savename' : 'figure_supp_deviations' }
 
-	colors = sns.color_palette( 'GnBu', n_colors=3 )
+	colors = sns.color_palette( 'Paired', n_colors=3 )
 
 
 	## DATA ##
@@ -103,7 +103,7 @@ if __name__ == "__main__":
 # A: Fractional variations in fitted parameters of bootstrapped model
 
 	# sel_datasets = [ dataname for dataname in fluxmean_data.index if datasets_openclosed[ dataname ] == 'open' ]
-	sel_datasets = [ 'AcademicRanking', 'AtlasComplex', 'Citations', 'Cities_RU', 'FIDEFemale', 'FIDEMale', 'Football_Scorers', 'Fortune', 'Golf_OWGR', 'Hienas', 'Nascar_BuschGrandNational', 'Nascar_WinstonCupGrandNational', 'Poker_GPI', 'Tennis_ATP', 'TheGuardian_numberComments', 'enron-sent-mails-weekly' ]
+	sel_datasets = [ 'AcademicRanking', 'AtlasComplex', 'Citations', 'Cities_RU', 'english', 'FIDEFemale', 'FIDEMale', 'Football_Scorers', 'Fortune', 'Golf_OWGR', 'Hienas', 'Nascar_BuschGrandNational', 'Nascar_WinstonCupGrandNational', 'Poker_GPI', 'Tennis_ATP', 'TheGuardian_numberComments', 'enron-sent-mails-weekly' ]
 
 	for grid_pos, dataname in enumerate( sel_datasets ): #loop through (open!) datasets (in order by decreasing mean flux)
 		print( 'flux = {:.2f}, dataset = {}'.format( fluxmean_data[ dataname ], dataname ) ) #to know where we stand
@@ -129,51 +129,61 @@ if __name__ == "__main__":
 
 		#statistical significance of deviations
 
-		#get rescaled model parameters (in data)
-		pnu_resc = ( pnu - p0 * open_deriv ) / open_deriv
-		ptau_resc = ptau / ( p0 * (1 - p0) * open_deriv )
+		#rescale bootstrapped parameters relative to original values
+		pnu_frac_devs = ( ( params_devs.pnu - pnu ) / pnu ).rename('pnu_frac')
+		ptau_frac_devs = ( ( params_devs.ptau - ptau ) / ptau ).rename('ptau_frac')
+		p0_frac_devs = ( ( params_devs.p0 - p0 ) / p0 ).rename('p0_frac')
+		data = pd.concat( [ pnu_frac_devs, ptau_frac_devs, p0_frac_devs ], axis=1 )
 
-		#get rescaled model parameters (in bootstrapped samples of model)
-		pnu_resc_devs = ( ( params_devs.pnu - params_devs.p0 * params_devs.open_deriv ) / params_devs.open_deriv ).rename('pnu_resc')
-		ptau_resc_devs = ( params_devs.ptau / ( params_devs.p0 * (1 - params_devs.p0) * params_devs.open_deriv ) ).rename('ptau_resc')
+		#correct model parameters with bootstrapped distribution
 
-		#get distance from universal curve (in data/samples)
-		curve = np.abs( np.log( ptau_resc * pnu_resc ) )
-		curve_devs = np.abs( np.log( ptau_resc_devs * pnu_resc_devs ) )
+		print( 'pnu, fitted = {:.4f}, corrected = {:.4f}'.format( pnu, 2*pnu - np.abs(params_devs.pnu.mean()) ) )
+		print( 'ptau, fitted = {:.4f}, corrected = {:.4f}'.format( ptau, 2*ptau - np.abs(params_devs.ptau.mean()) ) )
+		print( 'p0, fitted = {:.4f}, corrected = {:.4f}'.format( p0, 2*p0 - np.abs(params_devs.p0.mean()) ) )
 
-		#p-value as fraction of bootstrapped samples with larger distance than data's
-		pvalue = curve_devs[ curve_devs > curve ].size / float( curve_devs.size )
-		perror = 1 / ( 2 * np.sqrt( curve_devs.size ) )
-		print('\tp-value: {:.2f} +- {:.2f}'.format(pvalue, perror))
+
+		# #get rescaled model parameters (in data)
+		# pnu_resc = ( pnu - p0 * open_deriv ) / open_deriv
+		# ptau_resc = ptau / ( p0 * (1 - p0) * open_deriv )
+
+		# #get rescaled model parameters (in bootstrapped samples of model)
+		# pnu_resc_devs = ( ( params_devs.pnu - params_devs.p0 * params_devs.open_deriv ) / params_devs.open_deriv ).rename('pnu_resc')
+		# ptau_resc_devs = ( params_devs.ptau / ( params_devs.p0 * (1 - params_devs.p0) * params_devs.open_deriv ) ).rename('ptau_resc')
+		#
+		# #get distance from universal curve (in data/samples)
+		# curve = np.abs( np.log( ptau_resc * pnu_resc ) )
+		# curve_devs = np.abs( np.log( ptau_resc_devs * pnu_resc_devs ) )
+		#
+		# #p-value as fraction of bootstrapped samples with larger distance than data's
+		# pvalue = curve_devs[ curve_devs > curve ].size / float( curve_devs.size )
+		# perror = 1 / ( 2 * np.sqrt( curve_devs.size ) )
+		# print('\tp-value: {:.2f} +- {:.2f}'.format(pvalue, perror))
+
 
 		#plot plot!
 
 		#fitted parameters for data
 		plt.axvline( x=0, ls='--', c='0.5', lw=plot_props['linewidth'], zorder=1 )
 
-		#KDE/mean of bootstrapped model
-
-		pnu_frac_devs = ( ( params_devs['pnu'] - pnu ) / pnu ).rename('pnu_frac')
-		ptau_frac_devs = ( ( params_devs['ptau'] - ptau ) / ptau ).rename('ptau_frac')
-		p0_frac_devs = ( ( params_devs['p0'] - p0 ) / p0 ).rename('p0_frac')
-		data = pd.concat( [ pnu_frac_devs, ptau_frac_devs, p0_frac_devs ], axis=1 )
-
-		sns.kdeplot( data=data, x='pnu_frac', ax=ax, fill=True, color=colors[0], zorder=0 )
-		sns.kdeplot( data=data, x='ptau_frac', ax=ax, fill=True, color=colors[1], zorder=0 )
-		sns.kdeplot( data=data, x='p0_frac', ax=ax, fill=True, color=colors[2], zorder=0 )
+		#KDEs of bootstrapped model
+		sns.kdeplot( data=data, x='p0_frac', label='$p$', ax=ax, fill=True, color=colors[1], zorder=0 )
+		sns.kdeplot( data=data, x='ptau_frac', label=r'$\tau$', ax=ax, fill=True, color=colors[0], zorder=0 )
+		sns.kdeplot( data=data, x='pnu_frac', label=r'$\nu$', ax=ax, fill=True, color=colors[2], zorder=0 )
 
 		#texts
-		plt.text( 0.5, 1.25, datasets[ dataname ], va='center', ha='center', transform=ax.transAxes, fontsize=plot_props['ticklabel'] )
+		plt.text( 0.5, 1, datasets[ dataname ], va='bottom', ha='center', transform=ax.transAxes, fontsize=plot_props['ticklabel'] )
 
-		# pvalue_str = '$\Lambda =$ {:.2f} $\pm$ {:.2f}'.format( pvalue, perror )
-		# plt.text( 0.02, 0.99, pvalue_str, va='top', ha='left', transform=ax.transAxes, fontsize=plot_props['text_size'])
+		#legend
+		if grid_pos == 0:
+			leg = plt.legend( loc='lower left', bbox_to_anchor=(3, 1.25), prop=plot_props['legend_prop'], handlelength=1.7, numpoints=plot_props['legend_np'], columnspacing=plot_props['legend_colsp'], ncol=3 )
 
 		if grid_pos == 0:
 			plt.text( -0.68, 0.5, 'open', va='center', ha='center', transform=ax.transAxes, weight='bold', rotation='vertical', fontsize=plot_props['xylabel'] )
 			plt.annotate( text='', xy=( -0.68, -4.5 ), xytext=( -0.68, 0 ), arrowprops=dict(arrowstyle='<-', color='0.6'), xycoords=ax.transAxes, textcoords=ax.transAxes )
 
 		#finalise subplot
-		if grid_pos in [ 18, 19, 20, 21, 22, 23 ]:
+		# if grid_pos in [ 18, 19, 20, 21, 22, 23 ]:
+		if grid_pos in [ 12, 13, 14, 15, 16, 17 ]:
 			plt.xlabel( r'$\Delta \bullet$', size=plot_props['xylabel'], labelpad=2 )
 		else:
 			plt.xlabel('')
@@ -181,42 +191,11 @@ if __name__ == "__main__":
 			plt.ylabel( r'$P(\Delta \bullet)$', size=plot_props['xylabel'], labelpad=2 )
 		else:
 			plt.ylabel('')
-		# plt.axis([ -0.5, 0.6, -0.7, 0.5 ])
+		ax.set_xlim( -1.2*np.abs(data).max().max(), 1.2*np.abs(data).max().max() )
 		ax.locator_params( axis='both', nbins=3 )
 		ax.tick_params( axis='both', which='major', labelsize=plot_props['ticklabel'], pad=2 )
-		# if grid_pos not in [ 18, 19, 20, 21, 22, 23 ]:
-		# 	plt.xticks([])
-		# if grid_pos not in [ 0, 6, 12, 18 ]:
-		# 	plt.yticks([])
 
 	#finalise plot
 	if fig_props['savename'] != '':
 		plt.savefig( fig_props['savename']+'.pdf', format='pdf', dpi=fig_props['dpi'] )
 #		plt.savefig( fig_props['savename']+'.png', format='png', dpi=fig_props['dpi'] )
-
-
-#DEBUGGIN'
-
-	#universal curve in model
-
-	# pnu_resc_vals = np.logspace( -3, np.log10( 4e-1 ), 50 ) #pick rescaled pnu as variable
-	# ptau_resc_vals = 1 / pnu_resc_vals #and slide over universal curve
-
-		# params_resc_devs = pd.concat( [ pnu_resc_devs, ptau_resc_devs ], axis=1 ) #and join
-
-		#rescaled fitted parameters for data
-		# plt.loglog( pnu_resc, ptau_resc, 'o', label='data', c=datacols[datatypes[dataname]], ms=plot_props['marker_size'], zorder=2 )
-
-		# sns.kdeplot( data=params_resc_devs, x='pnu_resc', y='ptau_resc', ax=ax, log_scale=True, fill=True, palette='GnBu', zorder=1 )
-
-		#universal curve in model
-		# plt.loglog( pnu_resc_vals, ptau_resc_vals, '--', c='0.5', lw=plot_props['linewidth'], label=r'$\tau_r \nu_r = 1$', zorder=0 )
-
-			# plt.xlabel( r'$\nu_r$', size=plot_props['xylabel'], labelpad=2 )
-			# plt.ylabel( r'$\tau_r$', size=plot_props['xylabel'], labelpad=2 )
-		# curve = np.abs( ptau_resc - 1/pnu_resc )
-		# curve_devs = np.abs( ptau_resc_devs - 1/pnu_resc_devs )
-		# curve = np.abs( pnu_resc - 1/ptau_resc )
-		# curve_devs = np.abs( pnu_resc_devs - 1/ptau_resc_devs )
-		# curve = ( ptau_resc - 1/pnu_resc )**2 + ( pnu_resc - 1/ptau_resc )**2
-		# curve_devs = ( ptau_resc_devs - 1/pnu_resc_devs )**2 + ( pnu_resc_devs - 1/ptau_resc_devs )**2
